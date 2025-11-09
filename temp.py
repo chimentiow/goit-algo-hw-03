1e41bfb682165ae81a99e07f2344672d52a4eb0d
chimentiow <dimden55@gmail.com>
2025-11-09 09:09:49 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,9 +4,10 @@ max = int(input(f"Введіть максимальне число , яке бу
 quantity = int(input(f"Введіть скільки чисел повинно бути вибрані з діапазану від {min} до {max} "))
 
 def get_numbers_ticket(min, max, quantity):
-    if not isinstance(min , int) and isinstance(max , int) and isinstance(quantity , int) or min < 1 or max > 1000 or quantity < 1:
+    if not isinstance(min , int) and isinstance(max , int) and isinstance(quantity , int) or min < 1 or max > 1000 or quantity < 1 or max < min or quantity > (max-min):
         return []
     numbers = random.sample(range(min, max), quantity)
     return sorted(numbers)
+
 
 print ("Ваші лотерейні номерки :", (get_numbers_ticket(min , max , quantity)))
